1087480f893eb475e0fab86e42e6cd18a2f035fe
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2024-01-28 19:38:32 -0300

    adicionando cactos

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const dino = document.querySelector(".dino");
+const background = document.querySelector(".background");
 
 let isJumping = false;
+let position = 0;
 
 function handleKeyUp(event) {
   if (event.keyCode === 32) {
@@ -32,5 +34,31 @@ function jump() {
     }
   }, 20);
 }
+
+function createCactus() {
+  const cactus = document.createElement("div");
+  let cactusPosition = 1000;
+  let randomTime = Math.random() * 6000;
+
+  cactus.classList.add("cactus");
+  background.appendChild(cactus);
+  cactus.style.left = cactusPosition + "px";
+
+  let leftTimer = setInterval(() => {
+    if (cactusPosition < -60) {
+      clearInterval(leftTimer);
+      background.removeChild(cactus);
+    } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+      clearInterval(leftTimer);
+    } else {
+      cactusPosition -= 10;
+      cactus.style.left = cactusPosition + "px";
+    }
+  }, 20);
+
+  setTimeout(createCactus, randomTime);
+}
+
+createCactus();
 
 document.addEventListener("keyup", handleKeyUp);
